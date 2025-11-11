83b505beefb415c33788cc0278906f0cc4fda9a1
LDheldens <daniavici7895123@gmail.com>
2025-11-11 11:16:14 -0500

    feat(components/ui/ProgressBar.tsx)Se creo el componente para mostrar el progreso del fromualri de registro

---
+++ components/ui/ProgressBar.tsx
@@ -0,0 +1,37 @@
+import { theme } from '@/config/theme';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+interface ProgressBarProps {
+  steps: number;
+  currentStep: number;
+}
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({ steps, currentStep }) => {
+  const progress = (currentStep / steps) * 100;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.background}>
+        <View style={[styles.progress, { width: `${progress}%` }]} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: theme.spacing.md,
+  },
+  background: {
+    height: 4,
+    backgroundColor: theme.colors.border,
+    borderRadius: theme.borderRadius.full,
+    overflow: 'hidden',
+  },
+  progress: {
+    height: '100%',
+    backgroundColor: theme.colors.primary,
+    borderRadius: theme.borderRadius.full,
+  },
+});
