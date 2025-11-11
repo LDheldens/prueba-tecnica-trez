a5f69f7d656e86b5d309e9a50715fe0c28e8b6a9
LDheldens <daniavici7895123@gmail.com>
2025-11-11 10:55:00 -0500

    refactor(features/auth/services/authService.ts)Se agregó el servicio de register

--- features/auth/services/authService.ts
+++ features/auth/services/authService.ts
@@ -1,8 +1,10 @@
 import { apiClient } from '@/services/api/client';
 import { API_ENDPOINTS } from '@/services/api/endpoints';
-import { LoginCredentials, LoginResponse } from '../types/auth.types';
+import { formatBirthDate } from '@/utils/herpers';
+import { LoginCredentials, LoginResponse, RegisterFormData, RegisterRequest } from '../types/auth.types';
 
 export const authService = {
+  
   login: async (credentials: LoginCredentials): Promise<LoginResponse> => {
     return apiClient.post<LoginResponse>(
       API_ENDPOINTS.AUTH.LOGIN,
@@ -10,9 +12,29 @@ export const authService = {
     );
   },
 
-  // Para futuras implementaciones
-  register: async (data: any): Promise<LoginResponse> => {
-    return apiClient.post<LoginResponse>(API_ENDPOINTS.AUTH.REGISTER, data);
+  register: async (formData: RegisterFormData, acceptedTerms: boolean, acceptedDataPolicy: boolean): Promise<LoginResponse> => {
+    const registerData: RegisterRequest = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      birthDate: formatBirthDate(
+        formData.birthDate.day,
+        formData.birthDate.month,
+        formData.birthDate.year
+      ),
+      username: formData.username,
+      documentType: formData.documentType,
+      documentNumber: formData.documentNumber,
+      password: formData.password,
+      favoriteTeamId: formData.favoriteTeamId,
+      acceptedTerms,
+      acceptedDataPolicy,
+    };
+    
+    return apiClient.post<LoginResponse>(
+      API_ENDPOINTS.AUTH.REGISTER,
+      registerData
+    );
   },
 
   logout: async (token: string): Promise<void> => {
