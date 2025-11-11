646afb87b4cd47123521e124f99ab20371c4979e
LDheldens <daniavici7895123@gmail.com>
2025-11-11 10:55:39 -0500

    refactor(auth.types.ts)Se agreagron los types para el formulario de registro

--- features/auth/types/auth.types.ts
+++ features/auth/types/auth.types.ts
@@ -19,3 +19,50 @@ export interface AuthState {
   isLoading: boolean;
   error: string | null;
 }
+
+
+// types para el formulario de registro
+export interface RegisterStep1Data {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: {
+    day: string;
+    month: string;
+    year: string;
+  };
+}
+
+export interface RegisterStep2Data {
+  username: string;
+  documentType: 'DNI' | 'RUC';
+  documentNumber: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface RegisterStep3Data {
+  favoriteTeamId: number;
+}
+
+export interface RegisterFormData extends RegisterStep1Data, RegisterStep2Data, RegisterStep3Data {}
+
+export interface Team {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export interface RegisterRequest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: string; 
+  username: string;
+  documentType: string;
+  documentNumber: string;
+  password: string;
+  favoriteTeamId: number;
+  acceptedTerms: boolean;
+  acceptedDataPolicy: boolean;
+}
