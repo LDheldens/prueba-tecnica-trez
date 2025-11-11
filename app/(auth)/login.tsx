0fecabe0abc0fc0543e8de4e98d6aed82d326b00
LDheldens <daniavici7895123@gmail.com>
2025-11-11 06:52:36 -0500

    configuración inicial y definición de estructura de carpetas

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const LoginScren = () => {
+  return (
+    <View>
+      <Text>LoginScren</Text>
+    </View>
+  )
+}
+
+export default LoginScren
